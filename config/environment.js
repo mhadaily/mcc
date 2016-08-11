44f5bae3c22f37247e6de9c25e98544a580d7a14
yeechun beh <yeechun.beh@squarrific.com>
2016-08-11 16:33:47 +0800

    JSON API Adapter

--- config/environment.js
+++ config/environment.js
@@ -42,5 +42,9 @@ module.exports = function(environment) {
 
   }
 
+  ENV.apiHost = process.env.API_HOST || 'mobecentral.herokuapp.com'
+  ENV.apiPort = process.env.API_PORT ? ':' + process.env.API_PORT : ''
+  ENV.apiUrl = process.env.API_URL || `http://${ENV.apiHost}${ENV.apiPort}`;
+
   return ENV;
 };
