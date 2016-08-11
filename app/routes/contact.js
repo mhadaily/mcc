a9685e0162fde493c9896a5eb2965c0f3cb7f85a
Majid <mhadaily@gmail.com>
2016-08-11 19:04:48 +0800

    add blinker, contact page, add dl,dt,dd, change pre background to have contrast with bg change textarea and .form-contril to have contrast with bg

--- app/routes/contact.js
+++ app/routes/contact.js
@@ -1,4 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+	  model() {
+    return {
+      content: `karuna@mobe.com, you've got a new MTTB client to call:\n\n<a href=\"http://myonlinebusinessempire.com/info/index.php?lead=796593\"
+  target=\"_blank\">Click Here to Access the Dashboard</a>\n\nName: Rick Lamberson\nOffice
+  Phone: \nHome Phone: 9415266327\nCell Phone : 9415266327\nEmail: Dlrlamberson@gmail.com\nCountry:
+  \ United States\nCity and State:  Venice, Florida\nSkype: \n\nMTTB Username:  Dlrlamberson@gmail.com\nMTTB
+  Password:  2hLgum\n\n1. Get in contact with the customer with the details above\n2.
+  Complete the Relevant Form in the Dashboard\n<a href=\"http://myonlinebusinessempire.com/info/index.php?lead=796593\"
+  target=\"_blank\">Click Here to Access the Dashboard</a>\n3. Complete the Task with
+  the correct outcome.`
+    }
+  }
 });
