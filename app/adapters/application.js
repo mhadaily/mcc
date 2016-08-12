e9fb25bc54165eae78704220c5e68c1ef6132047
Simon Tseng <simon@simontseng.com>
2016-08-12 16:45:34 +0800

    DataAdapterMixin was imported but not included in the class definition

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 import config from '../config/environment';
 
-export default DS.JSONAPIAdapter.extend({
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin,{
   authorizer: 'authorizer:oauth2-bearer',
   host: config.apiUrl,
   namespace: 'api'
