b16784c4a531d8de75d96eb34aecc5ffd8beb0cd
Simon Tseng <simon@simontseng.com>
2016-08-14 14:36:04 +0800

    reference to product

--- app/models/sales-order.js
+++ app/models/sales-order.js
@@ -3,6 +3,7 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
   contact: DS.belongsTo(),
+  product: DS.belongsTo(),
   date: DS.attr(),
   amount: DS.attr(),
   source: DS.attr(),
