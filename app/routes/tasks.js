93027489f7ae640819dfc0080c54ba61ffa9f291
Majid <mhadaily@gmail.com>
2016-08-14 18:35:45 +0800

    Simple Pagination for Task,

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -2,7 +2,13 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model() {
-    return this.store.findAll('task');
+  queryParams:{
+  		page:{
+  			refreshModel: true
+  		}
+  },
+
+  model(params) {
+    return this.store.query('task',params);
   }
 });
