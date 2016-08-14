94cff70ddc0d1afcfcc8c6a12854186ce39a375e
Majid <mhadaily@gmail.com>
2016-08-14 19:36:03 +0800

    post Note to server in Task

---
+++ app/controllers/task.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+import config from '../config/environment';
+
+export default Ember.Controller.extend({
+
+	actions: {
+
+        newNote: function() {
+
+        var model = this.get('model');
+
+	    $.post(config.apiUrl+"/api/savenote", {
+
+	      contactReference: model.get("contact.reference"),
+	      noteText: this.get("noteContent"),
+
+
+	    }).then(function() {
+
+	      alert('Note has been saved');
+	      $(':input').val('');
+
+	    }, function() {
+
+	      alert('Saving Note Failed!');
+	      this.set("Failed", true);
+
+	    }.bind(this));
+	  }
+  }
+});
