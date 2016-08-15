896ec471835d12b6011fea91803b5aabe92444ce
Majid <mhadaily@gmail.com>
2016-08-15 18:06:43 +0800

    Contact new layout Contact Save note button,

---
+++ app/controllers/contact.js
@@ -0,0 +1,31 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+
+	actions: {
+
+        newNote: function() {
+
+        	var model = this.get('model');
+        	var newNoteData = {
+
+		      content: this.get('noteContent'),
+		      contact: model
+
+		    };
+
+        	model.store.createRecord('note', newNoteData).save().then(() => {
+
+		        alert('Note has been saved');
+		        this.set('noteContent', ' ');
+
+		    }, function() {
+
+			      alert('Saving Note Failed!');
+
+			     }
+		    );
+
+	  }
+  }
+});
