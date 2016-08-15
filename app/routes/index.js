ca8f63d76b0620a50b3f5c1043fc446e002a86e4
Simon Tseng <simon@simontseng.com>
2016-08-15 19:06:34 +0800

    fill in more summary data

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,21 @@
 import Ember from 'ember';
+import config from '../config/environment';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  model() {
+  model(params) {
+    let headers = {};
+
+    this.get('session').authorize('authorizer:oauth2-bearer', (headerName, headerValue) => {
+      headers[headerName] = headerValue;
+    });
+
     return Ember.RSVP.hash({
-      tasks: this.store.findAll('task')
+      tasks: this.store.findAll('task'),
+      summary: Ember.$.ajax(`${config.apiUrl}/analytics/phonereps`, {
+        headers: headers,
+        data: params
+      })
     });
   }
 });
