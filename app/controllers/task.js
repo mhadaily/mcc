4c2a197998d648a760ca189ef91bddfbfadc066e
Majid <mhadaily@gmail.com>
2016-08-15 14:23:20 +0800

    Remove $.post, prepare to user CreateRecord

--- app/controllers/task.js
+++ app/controllers/task.js
@@ -1,31 +1,31 @@
 import Ember from 'ember';
-import config from '../config/environment';
+// import config from '../config/environment';
 
 export default Ember.Controller.extend({
 
 	actions: {
 
-        newNote: function() {
+     //    newNote: function() {
 
-        var model = this.get('model');
+     //    var model = this.get('model');
 
-	    $.post(config.apiUrl+"/api/savenote", {
+	    // $.post(config.apiUrl+"/api/savenote", {
 
-	      contactReference: model.get("contact.reference"),
-	      noteText: this.get("noteContent"),
+	    //   contactReference: model.get("contact.reference"),
+	    //   noteText: this.get("noteContent"),
 
 
-	    }).then(function() {
+	    // }).then(function() {
 
-	      alert('Note has been saved');
-	      $(':input').val('');
+	    //   alert('Note has been saved');
+	    //   $(':input').val('');
 
-	    }, function() {
+	    // }, function() {
 
-	      alert('Saving Note Failed!');
-	      this.set("Failed", true);
+	    //   alert('Saving Note Failed!');
+	    //   this.set("Failed", true);
 
-	    }.bind(this));
-	  }
+	     // }.bind(this));
+	  // }
   }
 });
