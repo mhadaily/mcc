242fc29abada7d2016fb4ac16c823ddcd542d1b1
Majid <mhadaily@gmail.com>
2016-08-15 16:52:43 +0800

    bug fix

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,6 +2,6 @@ import Ember from 'ember';
 
 export default Ember.Controller.extend({
   dueTodayTasks: Ember.computed('model.tasks.[]', function() {
-    return this.get('model.tasks')
+    return this.get('model.tasks');
   })
 });
