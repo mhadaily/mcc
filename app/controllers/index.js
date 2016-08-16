596097757a3f7c6ab3348c57090844977de0f88c
Simon Tseng <simon@simontseng.com>
2016-08-16 17:56:42 +0800

    index tasks

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -3,5 +3,16 @@ import Ember from 'ember';
 export default Ember.Controller.extend({
   dueTodayTasks: Ember.computed('model.tasks.[]', function() {
     return this.get('model.tasks');
+  }),
+  overdueTasks: Ember.computed('model.tasks.[]', function() {
+    return this.get('model.tasks');
+  }),
+  futureTasks: Ember.computed('model.tasks.[]', function() {
+    return this.get('model.tasks');
+  }),
+  steps: Ember.computed('summary.steps',function() {
+    var steps = {}
+    for (i = 1; i <= 21; i++)
+    return steps;
   })
 });
