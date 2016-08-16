dcb72bd6efbb1a4fdca2756f483cdbfe0f31cb1b
yeechun beh <yeechun.beh@squarrific.com>
2016-08-16 18:30:12 +0800

    pagination for tasks

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -1,36 +1,13 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  // queryParams: [
-  //   'page',
-  // ],
-  // page: 1,
-  //
-  // actions: {
-  //   nextPage() {
-  //     let page = this.get('page');
-  //     this.set('page', page + 1);
-  //   },
-  //   prevPage() {
-  //     let page = this.get('page');
-  //     this.set('page', page - 1);
-  //   }
-  // },
-  queryParams: {
-    contact: {
-      refreshModel: true
-    },
-    subject: {
-      refreshModel: true
-    },
-    step: {
-      refreshModel: true
-    }
-  },
+  queryParams: ["contact", "subject", "step", "query", "page", "perPage"],
   contact: null,
   subject: null,
   step: null,
-  date_due_from: null,
+  query: '',
+  page: 1,
+  perPage: 25,
   sortBy: ['dateDue:desc'],
   tasks: Ember.computed.sort('model', 'sortBy')
 });
--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -1,11 +1,9 @@
 import Ember from 'ember';
+import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, {
+export default Ember.Route.extend(AuthenticatedRouteMixin, RouteMixin, {
   queryParams: {
-    page: {
-      refreshModel: true
-    },
     date_due_from: {
       refreshModel: true
     },
@@ -13,9 +11,9 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
       refreshModel: true
     }
   },
-  query: null,
   model(params) {
-    return this.store.query('task', {
+    return this.findPaged('task', {
+      paramMapping: {total_pages: "total-pages"},
       q: {
         contact_first_name_or_contact_last_name_cont: params.contact,
         subject_cont: params.subject,
