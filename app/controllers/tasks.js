85e785ceb5dd14bbf261262ffe29b0f9e8d645dd
yeechun beh <yeechun.beh@squarrific.com>
2016-08-16 16:04:13 +0800

    ransack filtering

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -1,19 +1,36 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: [
-    'page',
-  ],
-  page: 1,
-
-  actions: {
-    nextPage() {
-      let page = this.get('page');
-      this.set('page', page + 1);
+  // queryParams: [
+  //   'page',
+  // ],
+  // page: 1,
+  //
+  // actions: {
+  //   nextPage() {
+  //     let page = this.get('page');
+  //     this.set('page', page + 1);
+  //   },
+  //   prevPage() {
+  //     let page = this.get('page');
+  //     this.set('page', page - 1);
+  //   }
+  // },
+  queryParams: {
+    contact: {
+      refreshModel: true
     },
-    prevPage() {
-      let page = this.get('page');
-      this.set('page', page - 1);
+    subject: {
+      refreshModel: true
+    },
+    step: {
+      refreshModel: true
     }
-  }
+  },
+  contact: null,
+  subject: null,
+  step: null,
+  date_due_from: null,
+  sortBy: ['dateDue:desc'],
+  tasks: Ember.computed.sort('model', 'sortBy')
 });
--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -2,13 +2,32 @@ import Ember from 'ember';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
-  queryParams:{
-  		page:{
-  			refreshModel: true
-  		}
+  queryParams: {
+    page: {
+      refreshModel: true
+    },
+    date_due_from: {
+      refreshModel: true
+    },
+    date_due_to: {
+      refreshModel: true
+    }
   },
-
+  query: null,
   model(params) {
-    return this.store.query('task',params);
+    return this.store.query('task', {
+      q: {
+        contact_first_name_or_contact_last_name_cont: params.contact,
+        subject_cont: params.subject,
+        step_num_eq: params.step,
+        date_due_gteq: params.date_due_from,
+        date_due_lteq: params.date_due_to
+      }
+    });
+  },
+  actions: {
+    queryChanged() {
+      this.refresh();
+    }
   }
 });
