fd0c5c116028bd7d0f6e43c058e999c693be3a0e
Simon Tseng <simon@simontseng.com>
2016-08-21 18:58:15 +0800

    redirect to index if isAuthenticated

--- app/routes/login.js
+++ app/routes/login.js
@@ -1,6 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
-  session: Ember.inject.service()
-
+  session: Ember.inject.service(),
+  beforeModel() {
+    if(this.get('session.isAuthenticated')) {
+      this.transitionTo('index');
+    }
+  }
 });
