645eda749372835a42a7f85d9cd2a95203b0ceba
yeechun beh <yeechun.beh@squarrific.com>
2016-08-21 13:58:30 +0800

    fixed date picked which is not displaying; added search button; using ember-cli-datepicker which also support bootstrap

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -1,9 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ["contact", "subject", "step", "query", "page", "perPage"],
+  queryParams: ["contact", "subject", "date_due_gteq", "date_due_lteq", "step", "query", "page", "perPage"],
   contact: null,
   subject: null,
+  date_due_gteq: null,
+  date_due_lteq: null,
+  date: null,
   step: null,
   query: '',
   page: 1,
--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -3,14 +3,6 @@ import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, RouteMixin, {
-  queryParams: {
-    date_due_from: {
-      refreshModel: true
-    },
-    date_due_to: {
-      refreshModel: true
-    }
-  },
   model(params) {
     return this.findPaged('task', {
       paramMapping: { total_pages: "total-pages" },
@@ -18,8 +10,8 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, RouteMixin, {
         contact_first_name_or_contact_last_name_cont: params.contact,
         subject_cont: params.subject,
         step_num_eq: params.step,
-        date_due_gteq: params.date_due_from,
-        date_due_lteq: params.date_due_to
+        date_due_gteq: params.date_due_gteq,
+        date_due_lteq: params.date_due_lteq
       }
     });
   },
