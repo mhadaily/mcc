a81f39317c5b792d868272c6f625c717def18ab3
Majid <mhadaily@gmail.com>
2016-08-22 17:29:25 +0800

    Jshint bug fix

--- app/components/link-to-with-tip/component.js
+++ app/components/link-to-with-tip/component.js
@@ -2,10 +2,10 @@ import Ember from 'ember';
 
 export default Ember.LinkComponent.extend({
   didInsertElement() {
-    this._super(...arguments)
-    $(this.element).find('a').tooltip();
-    $(this.element).find('a').on('click',() => {
-      $(this.element).find('a').tooltip('hide')
-    })
+    this._super(...arguments);
+    this.$(this.element).find('a').tooltip();
+    this.$(this.element).find('a').on('click', () => {
+      this.$(this.element).find('a').tooltip('hide');
+    });
   }
 });
