b38d7cff40df7cbe1e6311ba8cf5d5feeaa8ee39
Simon Tseng <simon@simontseng.com>
2016-08-22 17:49:13 +0800

    show order status

--- app/models/sales-order.js
+++ app/models/sales-order.js
@@ -5,6 +5,7 @@ export default DS.Model.extend({
   contact: DS.belongsTo(),
   product: DS.belongsTo(),
   date: DS.attr(),
+  status: DS.attr(),
   amount: DS.attr(),
   source: DS.attr(),
   reference: DS.attr()
