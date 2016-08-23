cb81a4b23d55bedf6713378980ec2594b87e9218
yeechun beh <yeechun.beh@squarrific.com>
2016-08-23 17:55:47 +0800

    add status column and filter by status

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -1,11 +1,12 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ["contact", "subject", "date_due_gteq", "date_due_lteq", "query", "sort", "sortDir"],
+  queryParams: ["contact", "subject", "date_due_gteq", "date_due_lteq", "status_eq", "query", "page", "perPage", "sort", "sortDir"],
   contact: null,
   subject: null,
   date_due_gteq: null,
   date_due_lteq: null,
+  status_eq: null,
   query: '',
   sort: '',
   sortDir: 'asc',
