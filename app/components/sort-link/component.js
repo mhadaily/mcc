086db1640dac66bf171e8f9d862c61cf3d6342a9
Simon Tseng <simon@simontseng.com>
2016-08-23 15:00:46 +0800

    unfinished sort links

---
+++ app/components/sort-link/component.js
@@ -0,0 +1,18 @@
+import Ember from 'ember';
+
+const SortLinkComponent = Ember.LinkComponent.extend({
+  tagName: 'th',
+  classNameBindings: ['header','isAsc:headerSortUp:headerSortDown'],
+  name: null,
+  sort: null,
+  sortDir: 'desc',
+  isAsc: Ember.computed('sortDir',function() {
+    return this.sortDir == 'asc'
+  })
+});
+
+// SortLinkComponent.reopenClass({
+//   positionalParams: ['name', 'sort', 'sortDir']
+// });
+
+export default SortLinkComponent;
