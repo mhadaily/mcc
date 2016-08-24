f647e2c2ca82874cb935aa6043fb185325e3ebf9
yeechun beh <yeechun.beh@squarrific.com>
2016-08-24 23:46:53 +0800

    Tasks - drop down for status(default to pending)

--- app/controllers/tasks.js
+++ app/controllers/tasks.js
@@ -1,13 +1,16 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  statuses: [
+    'pending', 'completed', 'cancelled'
+  ],
   queryParams: ["contact", "contact_step", "subject", "date_due_gteq", "date_due_lteq", "status_eq", "query", "page", "perPage", "sort", "sortDir"],
   contact: null,
   contact_step: null,
   subject: null,
   date_due_gteq: null,
   date_due_lteq: null,
-  status_eq: null,
+  status_eq: 'pending',
   query: '',
   sort: '',
   sortDir: 'asc'
