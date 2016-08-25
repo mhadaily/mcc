dfacdd84771dd5b8e8ac0a02194d7e7e8e43f325
Simon Tseng <simon@simontseng.com>
2016-08-25 19:23:31 +0800

    normalise contact to first level

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -4,6 +4,9 @@ export default Ember.Controller.extend({
   queryParams: ['backTo', 'step'],
   step: null,
   backTo: null,
+  contact: Ember.computed('model',function() {
+    return this.get('model');
+  }),
   notes: Ember.computed('model.notes.[]','model.notes.@each.date',function() {
     return this.model.get('notes').sortBy('date').reverse();
   })
--- app/controllers/task.js
+++ app/controllers/task.js
@@ -5,6 +5,9 @@ export default Ember.Controller.extend({
   step: null,
   taskStatus: null,
   backTo: null,
+  contact: Ember.computed('model.contact',function() {
+    return this.get('model.contact');
+  }),
   notes: Ember.computed('model.contact.notes.[]','model.contact.notes.@each.date',function() {
     return this.model.get('contact.notes').sortBy('date').reverse();
   })
