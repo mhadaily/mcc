4995175dfa31f97ee4695c079438ca0b340341ac
Simon Tseng <simon@simontseng.com>
2016-08-26 19:37:31 +0800

    use same partial as tasks

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -7,6 +7,12 @@ export default Ember.Controller.extend({
   contact: Ember.computed('model',function() {
     return this.get('model');
   }),
+  tasks: Ember.computed('model.tasks.[]','model.tasks.@each.date',function() {
+    return this.get('model.tasks').sortBy('dateDue').reverse();
+  }),
+  salesOrders: Ember.computed('model.salesOrders.[]','model.salesOrders.@each.date',function() {
+    return this.get('model.salesOrders').sortBy('date').reverse();
+  }),
   notes: Ember.computed('model.notes.[]','model.notes.@each.date',function() {
     return this.model.get('notes').sortBy('date').reverse();
   })
