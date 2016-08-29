eb5bccc0c82f4d55dc889a7721b7aa7465030fd0
Majid <mhadaily@gmail.com>
2016-08-29 19:17:48 +0800

    bug fix

--- app/controllers/contact.js
+++ app/controllers/contact.js
@@ -4,16 +4,26 @@ export default Ember.Controller.extend({
   queryParams: ['backTo', 'step'],
   step: null,
   backTo: null,
-  contact: Ember.computed('model',function() {
+  blink: null,
+  contact: Ember.computed('model', function() {
     return this.get('model');
   }),
-  tasks: Ember.computed('model.tasks.[]','model.tasks.@each.date',function() {
+  tasks: Ember.computed('model.tasks.[]', 'model.tasks.@each.date', function() {
     return this.get('model.tasks').sortBy('dateDue').reverse();
   }),
-  salesOrders: Ember.computed('model.salesOrders.[]','model.salesOrders.@each.date',function() {
+  salesOrders: Ember.computed('model.salesOrders.[]', 'model.salesOrders.@each.date', function() {
     return this.get('model.salesOrders').sortBy('date').reverse();
   }),
-  notes: Ember.computed('model.notes.[]','model.notes.@each.date',function() {
+  notes: Ember.computed('model.notes.[]', 'model.notes.@each.date', function() {
     return this.model.get('notes').sortBy('date').reverse();
-  })
+  }),
+  actions: {
+    taskChangeColor: function() {
+      this.set('blink', 'blinker');
+      // remove blinker after 1 sec, it must be passed to reference for 'this' so easily we can bind that.
+      setTimeout(function() {
+        this.set('blink', ' ');
+      }.bind(this), 1000);
+    }
+  }
 });
