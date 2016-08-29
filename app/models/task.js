5f628ada39e8cb7a877339c53dce79e4bcb33c8e
Simon Tseng <simon@simontseng.com>
2016-08-29 19:10:10 +0800

    display overdue task in different color

--- app/models/task.js
+++ app/models/task.js
@@ -16,5 +16,8 @@ export default DS.Model.extend({
   stepNum: DS.attr(),
   dateAssigned: DS.attr('date'),
   dateDue: DS.attr('date'),
-  dateComplete: DS.attr('date')
+  dateComplete: DS.attr('date'),
+  isOverdue: Ember.computed('dateDue',function() {
+    return this.get('dateDue') < new Date();
+  })
 });
