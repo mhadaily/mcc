181ee64d58d8b44691d89c6e24370a4c00ef4ebf
Majid <mhadaily@gmail.com>
2016-08-29 15:35:33 +0800

    breakline helper

--- app/helpers/break-line.js
+++ app/helpers/break-line.js
@@ -3,7 +3,7 @@ import Ember from 'ember';
 export function breakLine(param) {
 
   var text = Ember.String.htmlSafe(param);
-  text = text.toString().replace(/(\r\n|\n|\r)/gm, '<p></p>');
+  text = text.toString().replace(/(\r\n|\n|\r)/gm, '<br>');
   return Ember.String.htmlSafe(text);
 
 }
