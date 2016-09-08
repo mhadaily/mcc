75223fe41cdac516ebf8d4dfe8ac666c43fed2a1
Majid <mhadaily@gmail.com>
2016-09-08 17:37:39 +0800

    remove ss from timezone clock

--- app/components/clock-timezone.js
+++ app/components/clock-timezone.js
@@ -6,16 +6,20 @@ let timer;
 export default Ember.Component.extend({
   tagName: 'span',
   time: null,
-  format: 'hh:mm:ssa',
+  format: 'hh:mm a',
   timezone: null,
-
+  interval: 60000,
+  init() {
+    this._super(...arguments);
+    this.set('time', moment.tz(this.get('timezone')).format(this.get('format')));
+  },
   didInsertElement() {
     this._super(...arguments);
 
     timer = setInterval(function() {
       this.set('time', new Date());
       this.set('time', moment.tz(this.get('timezone')).format(this.get('format')));
-    }.bind(this), 1000);
+    }.bind(this), this.get('interval'));
   },
   willDestroyElement() {
     this._super(...arguments);
