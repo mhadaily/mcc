bd9d9f3d5e43fb9e083225a2b5c73532a206c9bd
Majid <mhadaily@gmail.com>
2016-09-13 17:17:37 +0800

    Help-hover component done

--- app/components/hover-help/component.js
+++ app/components/hover-help/component.js
@@ -1,5 +1,14 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-  tag: ['div']
+  tagName: 'span',
+  title: null,
+  didInsertElement(title) {
+    this._super(...arguments);
+    this.set('title', title);
+    this.$(this.element).find('a').tooltip();
+    this.$(this.element).find('a').on('click', () => {
+      this.$(this.element).find('a').tooltip('hide');
+    });
+  }
 });
