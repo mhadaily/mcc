c049f97a4c23dea1e2d6d2c377012a777fa0918b
Majid <mhadaily@gmail.com>
2016-09-14 12:58:01 +0800

    tooltip refinement to show better size and proper z-index

--- app/components/hover-help/component.js
+++ app/components/hover-help/component.js
@@ -6,9 +6,6 @@ export default Ember.Component.extend({
   didInsertElement(title) {
     this._super(...arguments);
     this.set('title', title);
-    this.$(this.element).find('a').tooltip();
-    this.$(this.element).find('a').on('click', () => {
-      this.$(this.element).find('a').tooltip('hide');
-    });
+    this.$(this.element).find('a').tooltip({ container: 'body' });
   }
 });
