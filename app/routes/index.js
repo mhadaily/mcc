98a9f5cb1f08951e6b2ef28e933e37f3b8468437
Majid <mhadaily@gmail.com>
2016-09-14 12:35:29 +0800

    bug fix

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,8 +1,6 @@
 import Ember from 'ember';
-import config from '../config/environment';
 import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
-import moment from 'moment';
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, RouteMixin, {
   beforeModel() {
