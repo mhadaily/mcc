2add7b8656c572f8cb4860145f561e5b9257adf8
Majid <mhadaily@gmail.com>
2016-09-19 19:47:13 +0800

    Search Modal queryParams

--- app/components/search-modal.js
+++ app/components/search-modal.js
@@ -2,12 +2,21 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   classNames: ['search-modal'],
+  queryParams: ["contact", "step", "home_phone", "skype_id", "time_zone", "country_or_state"],
+  contact: '',
+  step: '',
+  home_phone: '',
+  skype_id: '',
+  time_zone: '',
+  country_or_state: '',
   actions: {
     dismiss() {
       this.sendAction('dismiss');
     },
-    queryChanged() {
-
+    searchNow() {
+      let email = this.get('contact')
+      console.log(email);
+      /* TODO - PASS TO CONTACTS ROUTE */
     }
   }
 });
