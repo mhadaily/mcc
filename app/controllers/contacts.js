bef77cd451c3284c570c2f7e863f1430897cc0fc
Majid <mhadaily@gmail.com>
2016-09-21 15:27:18 +0800

    contact search based on offset

--- app/controllers/contacts.js
+++ app/controllers/contacts.js
@@ -1,7 +1,7 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
-  queryParams: ["contact", "step", "home_phone", "cell_phone", "skype_id", "time_zone", "country_or_state", "query", "page", "perPage", "sort", "sortDir"],
+  queryParams: ["contact", "step", "offset_from", "offset_to", "home_phone", "cell_phone", "skype_id", "time_zone", "country_or_state", "query", "page", "perPage", "sort", "sortDir"],
   contact: '',
   step: '',
   home_phone: '',
@@ -12,5 +12,37 @@ export default Ember.Controller.extend({
   query: '',
   page: 1,
   sortBy: ['name:asc'],
-  contacts: Ember.computed.sort('model', 'sortBy')
+  contacts: Ember.computed.sort('model', 'sortBy'),
+  offset_from: null,
+  utcHourFromAction: Ember.computed('offset_from', {
+    get( /* key */ ) {
+      return this.get('offset_from') ? Ember.$.trim(this.get('offset_from')) : ' ';
+    },
+    set(key, value) {
+      let now = moment().utc().hour();
+      let diff = (value - now) * 60 * 60;
+      this.set('offset_from', value === 'N/A' ? null : diff);
+      return value;
+    }
+  }),
+  offset_to: null,
+  utcHourToAction: Ember.computed('offset_to', {
+    get( /* key */ ) {
+      return this.get('offset_to') ? Ember.$.trim(this.get('offset_to')) : ' ';
+    },
+    set(key, value) {
+      let now = moment().utc().hour();
+      let diff = (value - now) * 60 * 60;
+      this.set('offset_to', value === 'N/A' ? null : diff);
+      return value;
+    }
+  }),
+  utcHour: Ember.computed(function() {
+    let i = 0;
+    let arr = ['N/A'];
+    for (i; i <= 24; i++) {
+      arr.push(i);
+    }
+    return arr;
+  }),
 });
