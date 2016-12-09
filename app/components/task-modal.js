8b7e82e7919c691cd44f121a6ed740c72b321029
Majid <mhadaily@gmail.com>
2016-12-09 12:10:36 +0800

    Fix: complete task only if there is an outcome && note text

--- app/components/task-modal.js
+++ app/components/task-modal.js
@@ -9,13 +9,12 @@ export default Ember.Component.extend({
     return this.get('store').peekRecord('task', this.get('ref'));
   }),
   selectedOutcome: null,
-  init: function() {
+  init() {
     this._super(...arguments);
     if (!this.get('outcomeList')) {
       this.set('outcomeList', []);
     }
   },
-
   actions: {
     dismiss: function() {
       this.sendAction('dismiss');
@@ -25,23 +24,25 @@ export default Ember.Component.extend({
       let content = this.get('task.taskType.outcomes');
       let _selection = content[selectedIndex - 1];
       this.set('selectedOutcome', _selection);
-
     },
     complete: function(noteContentModal, selectedOutcome) {
       this.get('task').set('statusEvent', 'complete');
       this.get('task').set('outcome', selectedOutcome);
       this.get('task').set('note', noteContentModal);
 
-      this.get('task').save().then(d => {
-
-        this.get('notify').success('Task has been completed with following note :' + noteContentModal + ' and outcome is ' + selectedOutcome);
-        this.set('noteContentModal', ' ');
-        this.sendAction('dismiss');
-        return d;
-      }).catch(e => {
-        this.get('notify').error(e.message);
-        return e;
-      });
+      if (Ember.$.trim(noteContentModal) && selectedOutcome) {
+        this.get('task').save().then(d => {
+          this.get('notify').success('Task has been completed with following note :' + noteContentModal + ' and outcome is ' + selectedOutcome);
+          this.set('noteContentModal', ' ');
+          this.sendAction('dismiss');
+          return d;
+        }).catch(e => {
+          this.get('notify').error(e.message);
+          return e;
+        });
+      } else {
+        this.get('notify').error('Please Select an Outcome and Write a Note before complete this task.');
+      }
 
     }
   }
