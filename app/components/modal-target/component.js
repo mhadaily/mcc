0514c27f8cfe6d671810cca7665bd06e559c3beb
Majid <mhadaily@gmail.com>
2017-01-04 12:24:07 +0800

    improvement: Light background for modals

--- app/components/modal-target/component.js
+++ app/components/modal-target/component.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   modalAnimation(){
     return this.lookup('explode').call(this, {
       pick: '.mobe-modal-background',
-      use: ['fade', { maxOpacity: 0.75 }]
+      use: ['fade', { maxOpacity: 0.45 }]
     }, {
       pick: '.mobe-modal-dialog',
       use: 'to-down'
