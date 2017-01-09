5ca99c0be12077c0265ad7cc639f5dc93af22ed9
Majid <mhadaily@gmail.com>
2017-01-09 19:34:43 +0800

    Add: contact log type

--- app/models/contact-log.js
+++ app/models/contact-log.js
@@ -1,8 +1,24 @@
+import Ember from 'ember';
 import DS from 'ember-data';
 
 export default DS.Model.extend({
-  contact:   DS.belongsTo(),
+  contact: DS.belongsTo(),
   reference: DS.attr(),
-  topic:     DS.attr(),
-  date:      DS.attr()
+  topic: DS.attr(),
+  contactLogType: DS.attr(),
+  date: DS.attr(),
+  translated: Ember.computed('contactLogType', function () {
+    let type = this.get('contactLogType');
+    switch (type) {
+      case '0':
+        type = "Email";
+        break;
+      case '1':
+        type = "Task";
+        break;
+      default:
+        return type;
+    }
+    return type;
+  })
 });
