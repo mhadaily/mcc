44d5e08dce21145b803040b257f5c36f3de863b3
Majid <mhadaily@gmail.com>
2017-01-25 17:16:58 +0800

    refactoring ajax request, cleaner code

--- app/components/funding-tag-modal/component.js
+++ app/components/funding-tag-modal/component.js
@@ -5,38 +5,35 @@ export default Ember.Component.extend({
   notify: Ember.inject.service('notify'),
   session: Ember.inject.service('session'),
   store: Ember.inject.service(),
-
   actions: {
-    processing(processing, disabled) {
+    processing(processing, disabled){
       Ember.$('#fundingButton').text(processing).attr('disabled', disabled);
     },
-    addTag(reference)
-    {
+    addTag(reference){
       this.actions.processing('Processing...', 'disabled');
       let headers = {};
       this.get('session').authorize('authorizer:oauth2-bearer', (headerName, headerValue) => {
         headers[headerName] = headerValue;
       });
+      const data = {
+        id: reference,
+        oap_funding_tag_id: 4126
+      };
       return Ember.$.ajax(config.apiUrl + '/api/contacts/add_tag', {
         type: "POST",
         headers: headers,
-        data: {
-          id: reference,
-          oap_funding_tag_id: 4126
-        }
-      }).then(data => {
-        this.actions.processing('Yes Submit', null);
-        this.dismiss();
-        this.get('store').pushPayload(data);
-        this.get('notify').success('You request was successfully added to the contact');
-      }).catch(e => {
-        this.actions.processing('Yes Submit', null);
-        this.dismiss();
-        this.get('notify').error('Unable to get data! at this time. ' + e.statusText);
-        return e;
-      });
-    }
-  }
-  ,
+        data: data
+      })
+        .then(res => {
+          this.get('store').pushPayload(res);
+          this.get('notify').success('You request was successfully added to the contact');
+        })
+        .catch(e => this.get('notify').error('Unable to get data! at this time. ' + e.statusText))
+        .done(() => {
+          this.dismiss();
+          this.actions.processing('Yes Submit', null);
+        });
+    },
+  },
 })
 ;
