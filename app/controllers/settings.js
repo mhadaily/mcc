9f711f7e46ed7d7aae520a11ce5b7f36aeefd20a
Majid <mhadaily@gmail.com>
2017-02-08 21:33:38 +0800

    saving user profile fields to current model and push back data.

--- app/controllers/settings.js
+++ app/controllers/settings.js
@@ -5,20 +5,4 @@ import timezones from '../utils/timezones';
 export default Ember.Controller.extend({
   timezones,
   countries,
-
-  actions: {
-    selectCountry(selection){
-      console.log(selection);
-    },
-    selectTimezone(selection){
-      console.log(selection);
-    },
-    savePassword(){
-      console.warn('Will be completed Soon');
-    },
-    saveProfile(){
-
-    },
-  }
-
 });
--- app/routes/settings.js
+++ app/routes/settings.js
@@ -3,6 +3,7 @@ import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-rout
 
 export default Ember.Route.extend(AuthenticatedRouteMixin, {
   session: Ember.inject.service(),
+  notify: Ember.inject.service('notify'),
   model: function () {
     return this.modelFor('application').account;
   },
@@ -10,5 +11,16 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, {
     invalidateSession() {
       this.get('session').invalidate();
     },
+    savePassword(){
+      console.warn('Will be completed Soon');
+    },
+    saveProfile(){
+      this.currentModel.save().then((d) => {
+        this.get('notify').success('Contact successfully updated');
+        return d;
+      }).catch((e) => {
+        this.get('notify').error(e.message);
+      });
+    }
   }
 });
