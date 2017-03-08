85d8849b8f58fa7ec63b87aa21c0a0f5dd6bca3d
Majid <mhadaily@gmail.com>
2017-03-08 18:09:22 +0800

    add state and country code as computed function

--- app/models/contact.js
+++ app/models/contact.js
@@ -1,5 +1,7 @@
 import Ember from 'ember';
 import DS from 'ember-data';
+import oapStates from '../utils/oapstates';
+import oapCountries from '../utils/oapcountries';
 
 export default DS.Model.extend({
   notes: DS.hasMany(),
@@ -48,6 +50,22 @@ export default DS.Model.extend({
   mttbStepUncontactable: DS.attr(),
   silverAgreementSigned: DS.attr(),
   mlrDate: DS.attr(),
+  stateCode: Ember.computed('state', function () {
+    const state = this.get('state');
+    const stateCode = oapStates.filter(oapState => oapState.text === state);
+    if (stateCode.length === 1) {
+      return stateCode[0].value;
+    }
+    return state;
+  }),
+  countryCode: Ember.computed('country', function () {
+    const country = this.get('country');
+    const countryCode = oapCountries.filter(oapCountry => oapCountry.text === country);
+    if (countryCode.length === 1) {
+      return countryCode[0].value;
+    }
+    return country;
+  }),
   agreementTags: Ember.computed('tags', function () {
     let tags = this.get('tags'),
       result = {};
