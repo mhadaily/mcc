c607e1a96d8f4e2f6b30d06f7dc5966398eaffbe
Majid <mhadaily@gmail.com>
2017-03-08 14:56:38 +0800

    OAP counties json

---
+++ tests/unit/utils/oapcountries-test.js
@@ -0,0 +1,10 @@
+import oapcountries from 'mobecallcentre/utils/oapcountries';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | oapcountries');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = oapcountries();
+  assert.ok(result);
+});
