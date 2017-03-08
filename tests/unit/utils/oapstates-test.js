a9a0a86c954c52b400f6af794e991edab9a45de4
Majid <mhadaily@gmail.com>
2017-03-08 14:40:53 +0800

    OAP State as json

---
+++ tests/unit/utils/oapstates-test.js
@@ -0,0 +1,10 @@
+import oapstates from 'mobecallcentre/utils/oapstates';
+import { module, test } from 'qunit';
+
+module('Unit | Utility | oapstates');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = oapstates();
+  assert.ok(result);
+});
