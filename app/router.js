58aaca7c5d78e6b712b7faab007c88ad55a17f4e
Majid <mhadaily@gmail.com>
2017-03-29 12:16:03 +0800

    route names and stick on ES7

--- app/router.js
+++ app/router.js
@@ -1,26 +1,26 @@
 import Ember from 'ember';
 import config from './config/environment';
+const { Router, inject, run } = Ember;
 
-const Router = Ember.Router.extend({
+export default Router.extend({
   location: config.locationType,
-  metrics: Ember.inject.service(),
+  metrics: inject.service(),
   rootURL: config.rootURL,
-
+  
   didTransition() {
     this._super(...arguments);
     this._trackPage();
   },
-
+  
   _trackPage() {
-    Ember.run.scheduleOnce('afterRender', this, () => {
+    run.scheduleOnce('afterRender', this, () => {
       const page = this.get('url');
       const title = this.getWithDefault('currentRouteName', 'unknown');
-
+      
       Ember.get(this, 'metrics').trackPage({ page, title });
     });
-  }
+  },
 });
-
 
 Router.map(function() {
   this.route('dashboard');
@@ -29,13 +29,12 @@ Router.map(function() {
   this.route('contacts');
   this.route('contact', { path: 'contacts/:contact_id' }, function() {
     this.route('summary');
-    this.route('contact-details');
-    this.route('contact-tasks');
-    this.route('sale-activities');
-    this.route('task-activities');
-    this.route('contact-tags');
-    this.route('contact-application');
-    this.route('contact-logs');
+    this.route('details');
+    this.route('sales');
+    this.route('calls');
+    this.route('tags');
+    this.route('logs');
+    this.route('interview');
   });
   this.route('settings');
   this.route('login');
@@ -46,5 +45,3 @@ Router.map(function() {
   this.route('l2resources');
   this.route('calendar');
 });
-
-export default Router;
