f961b184fb1575e4341c2d144ca25109af6b0e60
Majid <mhadaily@gmail.com>
2017-03-29 14:40:23 +0800

    set currentUser id

--- app/routes/contact/calls/show.js
+++ app/routes/contact/calls/show.js
@@ -5,4 +5,8 @@ export default Route.extend({
   model(params){
     return this.store.find('task', params.call_id);
   },
+  setupController(controller){
+    this._super(...arguments);
+    controller.set('currentUser', this.modelFor('application').account.id);
+  },
 });
