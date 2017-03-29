c789bb6955cab529b705c02573e8939bfc4372ee
Majid <mhadaily@gmail.com>
2017-03-29 14:39:57 +0800

    destructuring

--- app/routes/tasks.js
+++ app/routes/tasks.js
@@ -1,18 +1,20 @@
 import Ember from 'ember';
 import RouteMixin from 'ember-cli-pagination/remote/route-mixin';
 import AuthenticatedRouteMixin from 'ember-simple-auth/mixins/authenticated-route-mixin';
+const { Route } = Ember;
+const { trim } = Ember.$;
 
-export default Ember.Route.extend(AuthenticatedRouteMixin, RouteMixin, {
-
+export default Route.extend(AuthenticatedRouteMixin, RouteMixin, {
+  
   queryParams: {
-    sort: {refreshModel: true},
-    sortDir: {refreshModel: true},
-    date_due_gteq: {replace: true},
-    date_due_lteq: {replace: true},
-    offset_from: {replace: true},
-    offset_to: {replace: true}
+    sort: { refreshModel: true },
+    sortDir: { refreshModel: true },
+    date_due_gteq: { replace: true },
+    date_due_lteq: { replace: true },
+    offset_from: { replace: true },
+    offset_to: { replace: true },
   },
-
+  
   page: 1,
   perPage: 25,
   model(params) {
@@ -20,20 +22,20 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, RouteMixin, {
     const due_lteq = params.date_due_lteq ? params.date_due_lteq + ' 00:00:00' : params.date_due_lteq;
     let user_id = this.modelFor('application').account.get('id');
     return this.findPaged('task', {
-      paramMapping: {total_pages: "total-pages"},
+      paramMapping: { total_pages: 'total-pages' },
       q: {
-        contact_name_or_contact_email_or_contact_reference_cont: Ember.$.trim(params.contact),
-        contact_step_number_eq: Ember.$.trim(params.contact_step_number),
-        contact_time_zone_cont: Ember.$.trim(params.contact_time_zone),
-        subject_cont: Ember.$.trim(params.subject),
-        date_due_gteq: Ember.$.trim(due_gteq),
-        date_due_lt: Ember.$.trim(due_lteq),
-        contact_utc_offset_gteq: Ember.$.trim(params.offset_from),
-        contact_utc_offset_lt: Ember.$.trim(params.offset_to),
+        contact_name_or_contact_email_or_contact_reference_cont: trim(params.contact),
+        contact_step_number_eq: trim(params.contact_step_number),
+        contact_time_zone_cont: trim(params.contact_time_zone),
+        subject_cont: trim(params.subject),
+        date_due_gteq: trim(due_gteq),
+        date_due_lt: trim(due_lteq),
+        contact_utc_offset_gteq: trim(params.offset_from),
+        contact_utc_offset_lt: trim(params.offset_to),
         status_eq: (params.status_eq === 'all status' ? '' : params.status_eq),
         user_id_eq: (params.user_name === 'all calls' ? '' : user_id),
-        s: `${params.sort} ${params.sortDir}`
-      }
+        s: `${params.sort} ${params.sortDir}`,
+      },
     });
   },
   actions: {
@@ -41,8 +43,8 @@ export default Ember.Route.extend(AuthenticatedRouteMixin, RouteMixin, {
       this.refresh();
     },
     changePerPage(number){
-      this.set('perPage',number);
+      this.set('perPage', number);
       this.refresh();
-    }
-  }
+    },
+  },
 });
